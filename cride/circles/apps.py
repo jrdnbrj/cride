bf4a6c7144af9e58e5fd9f52df637fa26841ccba
Jordan Borja <www.jrdnbrj@gmail.com>
2021-01-23 01:14:18 -0500

    Circles Model and CSV Data

---
+++ cride/circles/apps.py
@@ -0,0 +1,11 @@
+"""Cricles app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class CirclesAppConfig(AppConfig):
+    """Circles app config."""
+
+    name = 'cride.circles'
+    verbose_name = 'Circles'
